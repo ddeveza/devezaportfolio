c63cc290c4ad759dc4a6f4fcf668e3811479afc9
Dennis Deveza <Dennis.Deveza@onsemi.com>
2021-01-10 16:12:55 +0800

    added navar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-   <h1>Hello Dennis</h1>
+    <Navbar/>
   );
 }
 
