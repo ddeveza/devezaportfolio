70090d35a17cf0e556aa28663129678a75364965
Dennis Deveza <Dennis.Deveza@onsemi.com>
2021-01-12 19:28:53 +0800

    Add navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import logo from '../logo.png'
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
             <div className="container">   
-                    <a className="navbar-brand" href="#"><img src="" alt="Logo"></img></a>
+                    <a className="navbar-brand" href="#"><img src={logo} alt="Logo" className="logo"></img></a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -15,7 +16,16 @@ const Navbar = () => {
                                 <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
+                                <a className="nav-link" href="#">About Me</a>
+                            </li>
+                            <li className="nav-item ">
+                                <a className="nav-link" href="#">Services <span className="sr-only">(current)</span></a>
+                            </li>
+                            <li className="nav-item ">
+                                <a className="nav-link" href="#">Portpolio <span className="sr-only">(current)</span></a>
+                            </li>
+                            <li className="nav-item ">
+                                <a className="nav-link" href="#">Contacts <span className="sr-only">(current)</span></a>
                             </li>
                         
                         </ul>
