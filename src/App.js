7122a10fa1b07aedc73e869599c30288f151daa9
Dennis Deveza <Dennis.Deveza@onsemi.com>
2021-01-14 20:52:54 +0800

    done with the header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Particles from "react-particles-js"
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Typed from 'react-typed';
+
 
 function App() {
   return (
-    <Navbar/>
+    <>
+      <Particles
+         params={{
+           particles:{
+             number:{
+                value:30,
+                density:{
+                  enable: true,
+                  value_area:900
+                }
+             },
+             shape:{
+               type:"square",
+               stroke:{
+                 width: 10,
+                 color: "#f9ab00"
+               }
+             }
+           }
+         }}
+      
+      />
+      <Navbar/>
+      <Header/>
+    </> 
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,13 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
             <div className="container">   
-                    <a className="navbar-brand" href="#"><img src={logo} alt="Logo" className="logo"></img></a>
+            <a className="navbar-brand" href="#"><img src={logo} alt="Logo" className="logo"></img></a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <FontAwesomeIcon icon={faBars} style={{color:"#fff"}}/>
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                   
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item">
                                 <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
